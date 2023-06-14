bb4689da2cf6d55fe7658e97cedc7808919d0ba4
Harsh Gupta <simpcoder02@gmail.com>
2023-06-14 16:28:44 +0530

    Updated backend server

--- backend/app.js
+++ backend/app.js
@@ -19,10 +19,10 @@ const user = require("./userRoute.js");
 const order = require("./orderRoutes.js");
 const payment = require("./paymentRoute.js");
 
-app.use(`${process.env.BASE_URL}/api/v1`, product);
-app.use(`${process.env.BASE_URL}/api/v1`, user);
-app.use(`${process.env.BASE_URL}/api/v1`, order);
-app.use(`${process.env.BASE_URL}/api/v1`, payment);
+app.use(`/api/v1`, product);
+app.use(`/api/v1`, user);
+app.use(`/api/v1`, order);
+app.use(`/api/v1`, payment);
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
